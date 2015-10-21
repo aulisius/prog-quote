0e6ab1c82d8a37fcb2178220fa680585a566af73
Faizaan <aulisius7@gmail.com>
2015-10-21 12:46:36 +0000

    random-js for generating random indices

---
+++ index.js
@@ -0,0 +1,60 @@
+var random = require("random-js")
+
+var authors = [
+    "Ken Thompson",
+    "Dennis Ritchie",
+    "John F. Woods",
+    "Linus Torvalds",
+    "Vint Cerf",
+    "Jon Skeet",
+    "Richard Mansfield",
+    "Doug McIlroy",
+    "Unknown",
+    "Edsger Dijkstra",
+    "Kelly Johnson",
+    "Chris Heilmann"
+    ]
+
+var quote = [
+    "When in doubt, use brute force.",
+    "Unix is simple. It just takes a genius to understand its simplicity.",
+    "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+    "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.", 
+    "There was something amazingly enticing about programming",
+    "At a deep level, all code is about communication: expressing ideas about what you want to achieve.",
+    "OOP is to writing a program, what going through airport security is to flying",
+    "As a programmer, it is your job to put yourself out of business. What you do today can be automated tomorrow.",
+    "Profanity is the one language all programmers know best.",
+    "Simplicity is prerequisite for reliability.",
+    "Keep it simple stupid",
+    "Java is to JavaScript what Car is to Carpet."
+    ]
+
+exports.quotes = function() { 
+    var index = random.integer(0, 11)
+    return { author : authors[index] , quote : quote[index] }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
